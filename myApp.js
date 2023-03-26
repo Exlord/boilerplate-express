f8d0d642aee8fb217402f029749f56ed0ed11f7c
Exlord <adeli.farhad@gmail.com>
2023-03-26 15:00:40 +0330

    chore :task 5

--- myApp.js
+++ myApp.js
@@ -4,6 +4,9 @@ app.use('/public', express.static('public'));
 app.get('/', function (req, res) {
   res.sendFile('views/index.html', {root: __dirname});
 });
+app.get('/json', function (req, res) {
+  res.json({"message": "Hello json"})
+});
 
 
 module.exports = app;
