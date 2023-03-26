39931cd74ae7ecf77243923eff7f4a944865b9cf
Exlord <adeli.farhad@gmail.com>
2023-03-26 14:50:08 +0330

    chore :task 3

--- myApp.js
+++ myApp.js
@@ -1,37 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/',function(req, res) {
-  res.sendFile('views/index.html' , { root : __dirname});
+app.get('/', function (req, res) {
+    res.sendFile('views/index.html', {root: __dirname});
   }
 )
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
