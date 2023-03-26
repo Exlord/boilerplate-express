a02742fd5c5abcc8450c56ff7cad1f53adb84c13
Exlord <adeli.farhad@gmail.com>
2023-03-26 14:55:38 +0330

    chore :task 4

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+app.use('/public', express.static('public'));
 app.get('/', function (req, res) {
   res.sendFile('views/index.html', {root: __dirname});
 });
