ac461bf6681773c3f9ab51d8fea49ed1956c24c5
Exlord <adeli.farhad@gmail.com>
2023-03-27 11:10:41 +0330

    chore :task 6

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 app.use('/public', express.static('public'));
@@ -5,7 +6,8 @@ app.get('/', function (req, res) {
   res.sendFile('views/index.html', {root: __dirname});
 });
 app.get('/json', function (req, res) {
-  res.json({"message": "Hello json"})
+  const msg = "Hello json";
+  res.json({"message": process.env.MESSAGE_STYLE === 'uppercase' ? msg.toUpperCase() : msg.toLowerCase()});
 });
 
 
